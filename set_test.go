6d0ce39388bc4cb27241786f63fd4df5df3f211d
matbur <mateusz@burniak.pl>
2017-12-19 19:22:13 +0100

    Add tests interface

---
+++ set_test.go
@@ -0,0 +1,33 @@
+package set
+
+import "testing"
+
+func TestSet_String(t *testing.T) {}
+
+func TestSet_Len(t *testing.T) {}
+
+func TestSet_Clear(t *testing.T) {}
+
+func TestSet_Copy(t *testing.T) {}
+
+func TestSet_Add(t *testing.T) {}
+
+func TestSet_IsIn(t *testing.T) {}
+
+func TestSet_Remove(t *testing.T) {}
+
+func TestSet_Pop(t *testing.T) {}
+
+func TestSet_Difference(t *testing.T) {}
+
+func TestSet_Intersection(t *testing.T) {}
+
+func TestSet_SymmetricDifference(t *testing.T) {}
+
+func TestSet_Union(t *testing.T) {}
+
+func TestSet_IsDisjoint(t *testing.T) {}
+
+func TestSet_IsSubset(t *testing.T) {}
+
+func TestSet_IsSuperset(t *testing.T) {}
