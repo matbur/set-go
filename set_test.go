5a1481e7a1ee933a8d8c9f99e18c6b44b080bc59
matbur <mateusz@burniak.pl>
2018-02-23 23:52:58 +0100

    Create new type of tests

--- set_test.go
+++ set_test.go
@@ -1,395 +1,552 @@
 package set
 
 import (
-	"errors"
-	"fmt"
 	"reflect"
 	"testing"
+	"fmt"
 )
 
 func TestNew(t *testing.T) {
-	for _, v := range []struct {
+	type args struct {
 		values []int
-		set    map[int]struct{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Set
 	}{
-		{[]int{}, map[int]struct{}{}},
-		{[]int{1}, map[int]struct{}{1: {}}},
-		{[]int{1, -2}, map[int]struct{}{1: {}, -2: {}}},
-		{[]int{1, 1}, map[int]struct{}{1: {}}},
-		{[]int{-1, 2, -1}, map[int]struct{}{-1: {}, 2: {}}},
-	} {
-		set, want := v.values, v.set
-		s := New(set...)
-		get := s.set
-		if !reflect.DeepEqual(get, want) {
-			t.Errorf("New(%v).set == %v, want %v", set, get, want)
-		}
+		{
+			"no values",
+			args{},
+			New(),
+		}, {
+			"one value",
+			args{[]int{3}},
+			New(3),
+		}, {
+			"two unique values",
+			args{[]int{1, -2}},
+			New(1, -2),
+		}, {
+			"three unique values",
+			args{[]int{0, 2, 3}},
+			New(0, 3, 2),
+		}, {
+			"one value two times",
+			args{[]int{-7, -7}},
+			New(-7),
+		}, {
+			"two values in three elements",
+			args{[]int{-1, 2 ,-1}},
+			New(2, -1),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.values...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Len(t *testing.T) {
-	for _, v := range []struct {
-		set []int
-		l   int
+	tests := []struct {
+		name string
+		s    *Set
+		want int
 	}{
-		{[]int{}, 0},
-		{[]int{1}, 1},
-		{[]int{1, -2}, 2},
-		{[]int{1, 1}, 1},
-		{[]int{-1, 2, -1}, 2},
-	} {
-		set, want := v.set, v.l
-		s := New(set...)
-		get := s.Len()
-		if get != want {
-			t.Errorf("New(%v).Len() == %v, want %v", set, get, want)
-		}
+		{
+			"no values",
+			New(),
+			0,
+		}, {
+			"one value",
+			New(3),
+			1,
+		}, {
+			"two unique values",
+			New(1, -2),
+			2,
+		}, {
+			"three unique values",
+			New(0, 3, 2),
+			3,
+		}, {
+			"one value two times",
+			New(-7, -7),
+			1,
+		}, {
+			"two values in three elements",
+			New(-1, 2, -1),
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Len(); got != tt.want {
+				t.Errorf("Set.Len() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Empty(t *testing.T) {
-	for _, v := range []struct {
-		set     []int
-		isEmpty bool
+	tests := []struct {
+		name string
+		s    *Set
+		want bool
 	}{
-		{[]int{}, true},
-		{[]int{1}, false},
-		{[]int{-1, 2, -1}, false},
-	} {
-		set, want := v.set, v.isEmpty
-		s := New(set...)
-		get := s.Empty()
-		if get != want {
-			t.Errorf("New(%v).Empty() == %v, want %v", set, get, want)
-		}
+		{
+			"no values",
+			New(),
+			true,
+		}, {
+			"one value",
+			New(3),
+			false,
+		}, {
+			"two unique values",
+			New(1, -2),
+			false,
+		}, {
+			"three unique values",
+			New(0, 3, 2),
+			false,
+		}, {
+			"one value two times",
+			New(-7, -7),
+			false,
+		}, {
+			"two values in three elements",
+			New(-1, 2, -1),
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Empty(); got != tt.want {
+				t.Errorf("Set.Empty() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Clear(t *testing.T) {
-	for _, v := range []struct {
-		set []int
+	tests := []struct {
+		name string
+		s    *Set
+		want *Set
 	}{
-		{[]int{}},
-		{[]int{1}},
-		{[]int{1, -2}},
-		{[]int{1, 1}},
-		{[]int{-1, 2, -1}},
-	} {
-		set := v.set
-		want := make(map[int]struct{})
-		s := New(set...)
-		s.Clear()
-		get := s.set
-		if !reflect.DeepEqual(get, want) {
-			t.Errorf("New(%v).Clear().set == %v, want %v", set, get, want)
-		}
+		{
+			"no values",
+			New(),
+			New(),
+		}, {
+			"one value",
+			New(3),
+			New(),
+		}, {
+			"two unique values",
+			New(1, -2),
+			New(),
+		}, {
+			"three unique values",
+			New(0, 3, 2),
+			New(),
+		}, {
+			"one value two times",
+			New(-7, -7),
+			New(),
+		}, {
+			"two values in three elements",
+			New(-1, 2, -1),
+			New(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.s.Clear()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Clear() = %v, want %v", got, tt.want)
+			}
+			got = tt.s
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Clear(); Set = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Copy(t *testing.T) {
-	for _, v := range []struct {
-		set []int
+	tests := []struct {
+		name string
+		s    *Set
+		want *Set
 	}{
-		{[]int{}},
-		{[]int{1}},
-		{[]int{1, -2}},
-		{[]int{1, 1}},
-		{[]int{-1, 2, -1}},
-	} {
-		set := v.set
-		s1 := New(set...)
-		s2 := s1.Copy()
-		addrS1 := fmt.Sprintf("%p", s1.set)
-		addrS2 := fmt.Sprintf("%p", s2.set)
-		if !reflect.DeepEqual(s1.set, s2.set) {
-			t.Errorf("New(%v).Copy() = %v, want %v", set, s2.set, s1.set)
-		}
-		if addrS1 == addrS2 {
-			t.Errorf("addr of New(%v).Copy() == %v is the same as original", set, addrS1)
-		}
+		{
+			"no values",
+			New(),
+			New(),
+		}, {
+			"one value",
+			New(3),
+			New(3),
+		}, {
+			"two unique values",
+			New(1, -2),
+			New(1, -2),
+		}, {
+			"three unique values",
+			New(0, 3, 2),
+			New(0, 3, 2),
+		}, {
+			"one value two times",
+			New(-7, -7),
+			New(-7),
+		}, {
+			"two values in three elements",
+			New(-1, 2, -1),
+			New(-1, 2),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.s.Copy()
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Copy() = %v, want %v", got, tt.want)
+			}
+			if fmt.Sprintf("%p", tt.s) == fmt.Sprintf("%p", got) {
+				t.Errorf("s1 = %v; s2 = s1.Copy(); s1 and s2 is the same object", tt.s)
+			}
+		})
 	}
 }
 
 func TestSet_Equal(t *testing.T) {
-	for _, v := range []struct {
-		set1    []int
-		set2    []int
-		isEqual bool
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want bool
 	}{
-		{[]int{}, []int{}, true},
-		{[]int{1}, []int{1}, true},
-		{[]int{1}, []int{7}, false},
-		{[]int{1, -2}, []int{1}, false},
-		{[]int{10, 2, -1}, []int{-1, 7}, false},
-		{[]int{2, -10}, []int{-10, 2}, true},
-	} {
-		set1, set2, want := v.set1, v.set2, v.isEqual
-		s := New(set1...)
-		get := s.Equal(New(set2...))
-		if get != want {
-			t.Errorf("New(%v).Equal(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+		{
+			"no values",
+			New(),
+			args{New()},
+			true,
+		}, {
+			"one value",
+			New(3),
+			args{New(3)},
+			true,
+		}, {
+			"two unique values",
+			New(1, -2),
+			args{New(-2, 1)},
+			true,
+		}, {
+			"three unique values",
+			New(0, 3, 2),
+			args{New(2, 3, 0)},
+			true,
+		}, {
+			"one value two times with one value",
+			New(-7),
+			args{New(-7, -7)},
+			true,
+		}, {
+			"two values in three elements",
+			New(2, -1),
+			args{New(-1, 2 ,-1)},
+			true,
+		}, {
+			"no values with one value",
+			New(2, -1),
+			args{New()},
+			false,
+		}, {
+			"one value with two values",
+			New(2),
+			args{New(-1, 2)},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Equal(tt.args.other); got != tt.want {
+				t.Errorf("Set.Equal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSet_ToSlice(t *testing.T) {
+	tests := []struct {
+		name string
+		s    *Set
+		want []int
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.ToSlice(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.ToSlice() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_String(t *testing.T) {
-	for _, v := range []struct {
-		set []int
-		str string
+	tests := []struct {
+		name string
+		s    *Set
+		want string
 	}{
-		{[]int{}, "{}"},
-		{[]int{1}, "{1}"},
-		{[]int{1, -2}, "{-2, 1}"},
-		{[]int{1, 1}, "{1}"},
-		{[]int{-1, 2, -1}, "{-1, 2}"},
-		{[]int{10, 2, -1}, "{-1, 2, 10}"},
-		{[]int{10, 2, -1, -10}, "{-10, -1, 2, 10}"},
-	} {
-		set, want := v.set, v.str
-		s := New(set...)
-		get := s.String()
-		if get != want {
-			t.Errorf("New(%v).String() == %v, want %v", set, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.String(); got != tt.want {
+				t.Errorf("Set.String() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Add(t *testing.T) {
-	for _, v := range []struct {
-		set   []int
-		value int
-		str   string
+	type args struct {
+		values []int
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want *Set
 	}{
-		{[]int{}, 7, "{7}"},
-		{[]int{1}, 7, "{1, 7}"},
-		{[]int{1, -2}, 1, "{-2, 1}"},
-		{[]int{10, 2, -1}, 7, "{-1, 2, 7, 10}"},
-		{[]int{10, 2, -1, -10}, 2, "{-10, -1, 2, 10}"},
-	} {
-		set, value, want := v.set, v.value, v.str
-		s := New(set...)
-		get := s.Copy().Add(value).String()
-		if get != want {
-			t.Errorf("New(%v).Add(%v) == %v, want %v", set, value, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Add(tt.args.values...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Add() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_IsIn(t *testing.T) {
-	for _, v := range []struct {
-		set   []int
+	type args struct {
 		value int
-		isIn  bool
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want bool
 	}{
-		{[]int{}, 7, false},
-		{[]int{1}, 7, false},
-		{[]int{1, -2}, 1, true},
-		{[]int{10, 2, -1}, 7, false},
-		{[]int{10, 2, -1, -10}, 2, true},
-	} {
-		set, value, want := v.set, v.value, v.isIn
-		s := New(set...)
-		get := s.IsIn(value)
-		if get != want {
-			t.Errorf("New(%v).IsIn(%v) == %v, want %v", set, value, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsIn(tt.args.value); got != tt.want {
+				t.Errorf("Set.IsIn() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Remove(t *testing.T) {
-	err := "no such value in set"
-	for _, v := range []struct {
-		set   []int
+	type args struct {
 		value int
-		err   error
+	}
+	tests := []struct {
+		name    string
+		s       *Set
+		args    args
+		wantErr bool
 	}{
-		{[]int{}, 7, errors.New(err)},
-		{[]int{1}, 7, errors.New(err)},
-		{[]int{1, -2}, 1, nil},
-		{[]int{10, 2, -1}, 7, errors.New(err)},
-		{[]int{10, 2, -1, -10}, 2, nil},
-	} {
-		set, value, want := v.set, v.value, v.err
-		s := New(set...)
-		get := s.Remove(value)
-		if (get == nil || want == nil ) && get != want ||
-			get != nil && want != nil && get.Error() != want.Error() {
-			t.Errorf(`New(%v).Remove(%v) == "%v", want "%v"`, set, value, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.s.Remove(tt.args.value); (err != nil) != tt.wantErr {
+				t.Errorf("Set.Remove() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
 
 func TestSet_Pop(t *testing.T) {
-	err := "the map is empty"
-	for _, v := range []struct {
-		set []int
-		err error
+	tests := []struct {
+		name    string
+		s       *Set
+		want    int
+		wantErr bool
 	}{
-		{[]int{}, errors.New(err)},
-		{[]int{1}, nil},
-		{[]int{1, -2}, nil},
-		{[]int{10, 2, -1}, nil},
-	} {
-		set, want := v.set, v.err
-		s := New(set...)
-		v, get := s.Pop()
-		if (get == nil || want == nil ) && get != want ||
-			get != nil && want != nil && get.Error() != want.Error() {
-			t.Errorf(`New(%v).Pop() == (%v, "%v"), want (x, "%v")`, set, v, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.s.Pop()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Set.Pop() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Set.Pop() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Difference(t *testing.T) {
-	for _, v := range []struct {
-		set1 []int
-		set2 []int
-		str  string
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want *Set
 	}{
-		{[]int{}, []int{}, "{}"},
-		{[]int{1}, []int{1}, "{}"},
-		{[]int{1}, []int{7}, "{1}"},
-		{[]int{1, -2}, []int{1}, "{-2}"},
-		{[]int{10, 2, -1}, []int{-1, 7}, "{2, 10}"},
-		{[]int{10, 2, -1, -10}, []int{-10, 2, 3}, "{-1, 10}"},
-	} {
-		set1, set2, want := v.set1, v.set2, v.str
-		s := New(set1...)
-		get := s.Difference(New(set2...)).String()
-		if get != want {
-			t.Errorf("New(%v).Difference(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Difference(tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Difference() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Intersection(t *testing.T) {
-	for _, v := range []struct {
-		set1 []int
-		set2 []int
-		str  string
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want *Set
 	}{
-		{[]int{}, []int{}, "{}"},
-		{[]int{1}, []int{1}, "{1}"},
-		{[]int{1}, []int{7}, "{}"},
-		{[]int{1, -2}, []int{1}, "{1}"},
-		{[]int{10, 2, -1}, []int{-1, 7}, "{-1}"},
-		{[]int{10, 2, -1, -10}, []int{-10, 2, 3}, "{-10, 2}"},
-	} {
-		set1, set2, want := v.set1, v.set2, v.str
-		s := New(set1...)
-		get := s.Intersection(New(set2...)).String()
-		if get != want {
-			t.Errorf("New(%v).Intersection(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Intersection(tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Intersection() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_Union(t *testing.T) {
-	for _, v := range []struct {
-		set1 []int
-		set2 []int
-		str  string
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want *Set
 	}{
-		{[]int{}, []int{}, "{}"},
-		{[]int{1}, []int{1}, "{1}"},
-		{[]int{1}, []int{7}, "{1, 7}"},
-		{[]int{1, -2}, []int{1}, "{-2, 1}"},
-		{[]int{10, 2, -1}, []int{-1, 7}, "{-1, 2, 7, 10}"},
-		{[]int{10, 2, -1, -10}, []int{-10, 2, 3}, "{-10, -1, 2, 3, 10}"},
-	} {
-		set1, set2, want := v.set1, v.set2, v.str
-		s := New(set1...)
-		get := s.Union(New(set2...)).String()
-		if get != want {
-			t.Errorf("New(%v).Union(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.Union(tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.Union() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_SymmetricDifference(t *testing.T) {
-	for _, v := range []struct {
-		set1 []int
-		set2 []int
-		str  string
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want *Set
 	}{
-		{[]int{}, []int{}, "{}"},
-		{[]int{1}, []int{1}, "{}"},
-		{[]int{1}, []int{7}, "{1, 7}"},
-		{[]int{1, -2}, []int{1}, "{-2}"},
-		{[]int{10, 2, -1}, []int{-1, 7}, "{2, 7, 10}"},
-		{[]int{10, 2, -1, -10}, []int{-10, 2, 3}, "{-1, 3, 10}"},
-	} {
-		set1, set2, want := v.set1, v.set2, v.str
-		s := New(set1...)
-		get := s.SymmetricDifference(New(set2...)).String()
-		if get != want {
-			t.Errorf("New(%v).SymmetricDifference(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.SymmetricDifference(tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Set.SymmetricDifference() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_IsDisjoint(t *testing.T) {
-	for _, v := range []struct {
-		set1       []int
-		set2       []int
-		isDisjoint bool
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want bool
 	}{
-		{[]int{}, []int{}, true},
-		{[]int{1}, []int{1}, false},
-		{[]int{1}, []int{7}, true},
-		{[]int{1, -2}, []int{1}, false},
-		{[]int{10, 2, -1}, []int{1, 7}, true},
-		{[]int{10, 2, -1, -10}, []int{-10, 2, 3}, false},
-	} {
-		set1, set2, want := v.set1, v.set2, v.isDisjoint
-		s := New(set1...)
-		get := s.IsDisjoint(New(set2...))
-		if get != want {
-			t.Errorf("New(%v).SymmetricDifference(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsDisjoint(tt.args.other); got != tt.want {
+				t.Errorf("Set.IsDisjoint() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_IsSubset(t *testing.T) {
-	for _, v := range []struct {
-		set1     []int
-		set2     []int
-		isSubset bool
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want bool
 	}{
-		{[]int{}, []int{}, true},
-		{[]int{1}, []int{1}, true},
-		{[]int{1}, []int{7}, false},
-		{[]int{1, -2}, []int{1}, false},
-		{[]int{10, 2, -1}, []int{1, 7}, false},
-		{[]int{-10, 2}, []int{10, 2, -1, -10}, true},
-	} {
-		set1, set2, want := v.set1, v.set2, v.isSubset
-		s := New(set1...)
-		get := s.IsSubset(New(set2...))
-		if get != want {
-			t.Errorf("New(%v).IsSubset(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsSubset(tt.args.other); got != tt.want {
+				t.Errorf("Set.IsSubset() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestSet_IsSuperset(t *testing.T) {
-	for _, v := range []struct {
-		set1     []int
-		set2     []int
-		isSubset bool
+	type args struct {
+		other *Set
+	}
+	tests := []struct {
+		name string
+		s    *Set
+		args args
+		want bool
 	}{
-		{[]int{}, []int{}, true},
-		{[]int{1}, []int{1}, true},
-		{[]int{1}, []int{7}, false},
-		{[]int{1, -2}, []int{1}, true},
-		{[]int{10, 2, -1}, []int{1, 2}, false},
-		{[]int{10, 2, -1, -10}, []int{-10, 2}, true},
-	} {
-		set1, set2, want := v.set1, v.set2, v.isSubset
-		s := New(set1...)
-		get := s.IsSuperset(New(set2...))
-		if get != want {
-			t.Errorf("New(%v).IsSuperset(New(%v)) == %v, want %v", set1, set2, get, want)
-		}
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.IsSuperset(tt.args.other); got != tt.want {
+				t.Errorf("Set.IsSuperset() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
